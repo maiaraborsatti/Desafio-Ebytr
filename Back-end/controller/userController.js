726b223381aff5bc585c97962a2d646f86d32188
Maiara Borsatti <maiara.borsatti@gmail.com>
2021-11-04 21:21:31 -0300

    criaçao do userController

---
+++ Back-end/controller/userController.js
@@ -0,0 +1,20 @@
+const userService = require('../services/userService');
+const auth = require('../auth/jwtFunctions');
+
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const result = await userService.validations({ email, password });
+
+    if (result.erro) {
+      return res.status(result.erro.code).json({ message: result.erro.message });
+    }
+
+    const token = auth.createJWT(result);
+    return res.status(200).json({ token });
+  } catch (error) {
+    return res.status(500).json({ message: 'Deu ruim' });
+  }
+};
+
+module.exports = { login };
--- Back-end/server.js
+++ Back-end/server.js
@@ -1,14 +1,19 @@
-
-  
 const express = require('express')
 const bodyParser = require('body-parser');
+
+const { authValidation } = require('./auth/authMiddleware');
+
+const userController = require('./controller/userController');
 
 
 const app = express();
 app.use(bodyParser.json());
 
 
-app.get('/', (request, response) => { response.send('estou aqui')});
+app.get('/', (request, response) => { response.send('estou')});
+
+app.route('/users')
+  .post(userController.login);
 
 const port = 3001;
 app.listen(port, () => console.log(`Listening on port ${port}!`))
